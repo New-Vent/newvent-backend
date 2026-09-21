457b454b79b2aa7a2cf4b93d59aa180b1a236245
kyd4947 <kyd4947@gmail.com>
2026-09-21 13:19:27 +0900

    feat : 4 - 일일 상한 초과 예외 코드

---
+++ src/main/java/com/newvent/common/error/LlmDailyLimitExceededException.java
@@ -0,0 +1,19 @@
+package com.newvent.common.error;
+
+import lombok.Getter;
+
+/**
+ * 일일 상한 초과 신호용 예외
+ */
+@Getter
+public class LlmDailyLimitExceededException extends RuntimeException {
+
+	private final int dailyLimit;
+	private final long used;
+	
+	public LlmDailyLimitExceededException(int dailyLimit, long used) {
+		super("LLM 일일 호출 상한 초과 : " + used + "/" + dailyLimit);
+		this.dailyLimit = dailyLimit;
+		this.used = used;
+	}
+}
